2bfbdf48283c9679074c9c4958224df75860bc72
oca-john <40886062+oca-john@users.noreply.github.com>
2019-10-22 18:21:53 +0800

    Create 安装zsh以及on_my_zsh脚本.sh

---
+++ Linux平台/安装zsh以及on_my_zsh脚本.sh
@@ -0,0 +1,14 @@
+## 安装zsh
+sudo zypper in zsh
+
+## 安装oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+## 修改默认shell
+chsh -s /bin/zsh
+
+## 将主体修改为agnoster
+vi .zshrc
+
+## 配色方案
+模拟终端颜色修改使得所有内容能看清
