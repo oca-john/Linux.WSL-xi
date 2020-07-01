379ae41e4df3cc14d9725b6e7d3a53a572f4a1c2
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-01 23:24:19 +0800

    Update WSL-openSUSE初始化.sh

--- Win-WSL平台/WSL-openSUSE初始化.sh
+++ Win-WSL平台/WSL-openSUSE初始化.sh
@@ -5,4 +5,12 @@ printf "softwares are being installed by repos\n"
 sudo zypper in cmake git python3-numpy python3-scipy python3-matplotlib python3-seaborn java-1_8_0-openjdk-devel
 printf "repos' softwares are installed\n"
 
+# configure the python3 and pip3
+sudo rm /usr/bin/python
+sudo ln -s python3.* /usr/bin/python
+sudo rm /usr/bin/pip
+sudo ln -s pip3 /usr/bin/pip
+# use `-i https://pypi.tuna.tsinghua.edu.cn/simple` to speed up the installation
+sudo pip install torch==1.5.1+cpu torchvision==0.6.1+cpu -i https://pypi.tuna.tsinghua.edu.cn/simple
+
 printf "everything's ok!\n"
