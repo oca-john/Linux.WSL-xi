c4d4d7c3b11e07a4f3def92f37ac20c5d9d1a5c7
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-02 05:13:38 +0000

    Update and rename 1.HelloWorld示例代码Bash.sh to BashScripts/1.hello.bash.sh

--- 1.HelloWorld示例代码Bash.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-
-echo "Hello, I'm printing Hello World by echo!"     # 第一种打印方式echo
-printf "Hello, I'm using printf to print strings."  # 第二种打印方式printf
---
+++ BashScripts/1.hello.bash.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+# print hello bash to screen
+
+echo "Hello, I'm printing Hello World by echo!\n"     # 第一种打印方式echo
+printf "Hello, I'm using printf to print strings.\n"  # 第二种打印方式printf
