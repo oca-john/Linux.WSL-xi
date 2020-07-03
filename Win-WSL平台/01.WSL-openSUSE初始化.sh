143cce1af70cc24ba78b5270cff375ffac89c33b
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-04 00:14:37 +0800

    Update 01.WSL-openSUSE初始化.sh

--- Win-WSL平台/01.WSL-openSUSE初始化.sh
+++ Win-WSL平台/01.WSL-openSUSE初始化.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/sh
 # WSL-openSUSE初始化
 
+# openSUSE命令中若直接带-y参数，只能跟在zypper的具体命令之后，不能放在最后
+# 如，`sudo zypper in -y emacs-nox`
+# 而Debian和CentOS中可以放在命令后，也可以放在对象列表的最后
+
 printf "softwares are being installed by repos\n"
-sudo zypper in cmake git python3-numpy python3-scipy python3-matplotlib python3-seaborn java-1_8_0-openjdk-devel
+sudo zypper in -y cmake git python3-numpy python3-scipy python3-matplotlib python3-seaborn java-1_8_0-openjdk-devel
 printf "repos' softwares are installed\n"
 
 # configure the python3 and pip3
