c712f7f8613ff05029f6c5cb7284fb926500e4da
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-03 22:57:22 +0800

    Update 01.WSL-openSUSE初始化.sh

--- Win-WSL平台/01.WSL-openSUSE初始化.sh
+++ Win-WSL平台/01.WSL-openSUSE初始化.sh
@@ -6,10 +6,12 @@ sudo zypper in cmake git python3-numpy python3-scipy python3-matplotlib python3-
 printf "repos' softwares are installed\n"
 
 # configure the python3 and pip3
-sudo rm /usr/bin/python
+cd /usr/bin/
+sudo rm python
 sudo ln -s python3.* /usr/bin/python
-sudo rm /usr/bin/pip
+sudo rm pip
 sudo ln -s pip3 /usr/bin/pip
+cd
 # use `-i https://pypi.tuna.tsinghua.edu.cn/simple` to speed up the installation
 sudo pip install torch==1.5.1+cpu torchvision==0.6.1+cpu -i https://pypi.tuna.tsinghua.edu.cn/simple
 printf "python3, pip3, pytorch are installed well\n"
