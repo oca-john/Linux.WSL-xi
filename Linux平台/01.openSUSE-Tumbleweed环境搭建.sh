65d14fa878aac440a72d5c02a32f450bf1844ac0
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-03 22:57:14 +0800

    Update 01.openSUSE-Tumbleweed环境搭建.sh

--- Linux平台/01.openSUSE-Tumbleweed环境搭建.sh
+++ Linux平台/01.openSUSE-Tumbleweed环境搭建.sh
@@ -10,10 +10,12 @@ sudo zypper in dia goldendict imagewriter kcolorchooser lyx marble plasma5-deskt
 printf "repos' softwares are installed\n"
 
 # configure the python3 and pip3
-sudo rm /usr/bin/python
+cd /usr/bin/
+sudo rm python
 sudo ln -s python3.* /usr/bin/python
-sudo rm /usr/bin/pip
+sudo rm pip
 sudo ln -s pip3 /usr/bin/pip
+cd
 # use `-i https://pypi.tuna.tsinghua.edu.cn/simple` to speed up the installation
 sudo pip install torch==1.5.1+cpu torchvision==0.6.1+cpu -i https://pypi.tuna.tsinghua.edu.cn/simple
 printf "python3, pip3, pytorch are installed well\n"
