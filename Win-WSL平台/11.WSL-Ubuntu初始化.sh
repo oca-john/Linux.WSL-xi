ca486d527934d4816fcecdaaa0d389a83e04f301
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-03 22:57:30 +0800

    Update 11.WSL-Ubuntu初始化.sh

--- Win-WSL平台/11.WSL-Ubuntu初始化.sh
+++ Win-WSL平台/11.WSL-Ubuntu初始化.sh
@@ -7,10 +7,12 @@ printf "repos' softwares are installed\n"
 # java 1.8 暂不支持apt直接安装
 
 # configure the python3 and pip3
-sudo rm /usr/bin/python
+cd /usr/bin/
+sudo rm python
 sudo ln -s python3.* /usr/bin/python
-sudo rm /usr/bin/pip
+sudo rm pip
 sudo ln -s pip3 /usr/bin/pip
+cd
 # use `-i https://pypi.tuna.tsinghua.edu.cn/simple` to speed up the installation
 sudo pip install torch==1.5.1+cpu torchvision==0.6.1+cpu -i https://pypi.tuna.tsinghua.edu.cn/simple
 printf "python3, pip3, pytorch are installed well\n"
