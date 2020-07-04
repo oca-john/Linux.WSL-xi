ce37182d4fabec0c11826fe1a384710e8b9124e6
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-04 23:42:18 +0800

    Update 01.openSUSE-Tumbleweed环境搭建.sh

--- Linux平台/01.openSUSE-Tumbleweed环境搭建.sh
+++ Linux平台/01.openSUSE-Tumbleweed环境搭建.sh
@@ -6,6 +6,7 @@
 # openSUSE命令中若直接带-y参数，只能跟在zypper的具体命令之后，不能放在最后
 # 如，`sudo zypper in -y emacs-nox`
 # 而Debian和CentOS中可以放在命令后，也可以放在对象列表的最后
+# 需写作，`sudo apt/dnf install -y emacs-nox`或`sudo apt/dnf install emacs-nox -y`
 
 # install from repos
 printf "softwares are being installed by repos\n"
