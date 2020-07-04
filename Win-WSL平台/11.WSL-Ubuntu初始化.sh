3290e748b3e39f07ce1822703100cb0a8cf98403
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-04 23:43:22 +0800

    Update 11.WSL-Ubuntu初始化.sh

--- Win-WSL平台/11.WSL-Ubuntu初始化.sh
+++ Win-WSL平台/11.WSL-Ubuntu初始化.sh
@@ -4,6 +4,7 @@
 # openSUSE命令中若直接带-y参数，只能跟在zypper的具体命令之后，不能放在最后
 # 如，`sudo zypper in -y emacs-nox`
 # 而Debian和CentOS中可以放在命令后，也可以放在对象列表的最后
+# 需写作，`sudo apt/dnf install -y emacs-nox`或`sudo apt/dnf install emacs-nox -y`
 
 printf "softwares are being installed by repos\n"
 sudo zypper in -y cmake git python3-numpy python3-scipy python3-matplotlib python3-seaborn
