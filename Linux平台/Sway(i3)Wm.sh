4631e3e8f60816f262187db7cb7e0234b610cdc6
oca-john <40886062+oca-john@users.noreply.github.com>
2020-07-15 00:48:16 +0800

    Create Sway(i3)Wm.sh

---
+++ Linux平台/Sway(i3)Wm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+# sway是i3窗口管理器的预配置版，增加了几个易用的工具，操作逻辑和语法上与i3完全一致。
+
+# 1. 安装
+sudo zypper in sway       # 安装sway
+# 或 sudo zypper in i3
+# 登录，直接在会话管理器中选择sway登录即可。
+# 退出，`Win + Shift + E`，表示Exit。
+
+# 2. 搜索程序打开
+# 搜索程序，`Win + D`，用`Enter`确认运行。
+
+# 3. 窗口关闭
+# `Win + Shift + Q`，关闭窗口
+# `Esc`，关闭对话框
+
+# 3. 布局
+# `Win + S`, `Win + W`, `Win +E`是三种窗口布局模式。
