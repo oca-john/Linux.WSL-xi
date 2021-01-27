52e01c9be56497433a9cec77035f2bc038e9d7fe
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-27 17:59:57 +0800

    Update 5.bash.learn.字符串处理.sh

--- BashScripts/5.bash.learn.字符串处理.sh
+++ BashScripts/5.bash.learn.字符串处理.sh
@@ -2,4 +2,10 @@
 
 
 # 访问数组、遍历数组、字符串选取、拼接、替换
-
+# Bash 只支持一维数组，用圆括号()包裹，元素之间用“空格”分割（不用逗号分割）
+# 直接初始化（赋值）
+arr_name=(value1 value2 value3 ... valueN)
+# 索引/下标初始化
+arr_name[0]=value1
+arr_name[1]=value2
+arr_name[12]=value13    # 不连续初始化（不会填充跳过的索引）
