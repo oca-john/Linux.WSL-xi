c18656e644e3aa71cb0cd8b4c5949ad322858989
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-27 15:45:20 +0800

    Update 3.bash.learn.流程控制.sh

--- BashScripts/3.bash.learn.流程控制.sh
+++ BashScripts/3.bash.learn.流程控制.sh
@@ -24,5 +24,41 @@ elif [ $a -gt $b ]
     echo "$a is greater than $b"
 else
     echo "$a is equal to $b"
-fi                              # 结尾用与 if 对应的 fi 结束
+fi                              # if 以 fi 结尾（if 的反拼）
+
+
+# 2. case 语句（多分支）
+case "${var}" in                # "" 的使用不强制
+    "$condition1")              # 测试行，以")"结尾
+        command1
+    ;;                          # 条件块，以";;"结尾
+    "$condition2")
+        command2
+    ;;
+    * )                         # 默认（无条件满足时输出此条），相当于 C 语言 case 中的 default
+        command_default
+    ;;
+esac                            # case 以 esac 结尾（case 的反拼）
+# 测试示例
+case $os in
+    Windows|Windows|Win|win)
+        echo "your platform is windows."
+    ;;
+    Linux|linux)
+        echo "oh! your platform is linux."
+    ;;
+    *)
+        echo "your platform is not clear."
+    ;;
+esac
+
+
+# 3. for 语句（数组循环）
+
+
+# 4. while 语句（条件循环）
+
+
+# 5. until 语句（条件循环）
+
 
