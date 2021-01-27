30dac79851b7676cd8c815c62a1f7b6d2ca97439
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-27 12:00:35 +0800

    Update 3.bash.learn.流程控制.sh

--- BashScripts/3.bash.learn.流程控制.sh
+++ BashScripts/3.bash.learn.流程控制.sh
@@ -9,17 +9,20 @@
 if [ condition1 ]               # 注意空格
 then
     command1
-elif [ condition2 ]             #
-then
+elif [ condition2 ]             # 支持多次嵌套
     command2
 else
     command3
-if
-# example
+fi
+# 测试示例
 a=3                             # 定义变量 = 两侧一定不能有空格
 b=4
 if [ $a -lt $b ]                # [] 和变量名直接一定要有空格，大小比较不能用 >, < （用于重定向）
 then
 	echo "$a is lower than $b"
+elif [ $a -gt $b ]
+    echo "$a is greater than $b"
+else
+    echo "$a is equal to $b"
 fi                              # 结尾用与 if 对应的 fi 结束
 
