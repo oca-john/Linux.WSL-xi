1b5a7ae7fbe780cc1d3c7af07d9dfda05d4d625b
oca-john <40886062+oca-john@users.noreply.github.com>
2021-01-28 10:58:02 +0800

    Update 5.bash.learn.字符串处理.sh

--- BashScripts/5.bash.learn.字符串处理.sh
+++ BashScripts/5.bash.learn.字符串处理.sh
@@ -32,3 +32,24 @@ str1='zhang'
 str2='san'
 str3=${str1}" "${str2}  # 使用空格连接即可（单词之间需要空格）
 echo ${str3}
+
+# 提取子字串
+str="test picking string"
+substr1=${str:3}            # 从 index=3 的元素开始提取其后所有元素
+substr2=${str:2:6}          # 从 index=2 的元素开始提取后面 6 个元素
+# 删除子字串
+str="abcabcdefabcabc"
+echo "${str#a*b}"           # 左(#)起，删除匹配 a*b 的最短子字串(模式左侧 = 起点元素 a)
+echo "${str##a*b}"          # 左(##)起，删除匹配 a*b 的最长子字串
+echo "${str%a*c}"           # 右(%)起，删除匹配 a*c 的最短子字串(模式右侧 = 末尾元素 c)
+echo "${str%%b*c}"          # 右(%%)起，删除匹配 b*c 的最长子字串
+# 替换子字串
+str="abcdefabc"
+echo ${str/"abc"/"FFF"}     # FFFdefabc, 替换第一处匹配项
+echo ${str//"abc"/"AAA"}    # AAAdefAAA, 替换所有匹配项
+echo ${str/#"abc"/"LLL"}    # LLLdefabc, 替换前缀匹配项
+echo ${str/%"abc"/"RRR"}    # abcdefabc, 替换后缀匹配项
+# 字串大小写
+str="abcDEfg"
+echo ${var^^}               # 转大写
+echo ${var,,}               # 转小写
