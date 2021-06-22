4cd6520542821af96051ad455cf91788dc2434fa
oca-john <40886062+oca-john@users.noreply.github.com>
2021-06-22 11:05:50 +0800

    Create sogou.install.on.opensuse.xi.sh

---
+++ Linux平台/sogou.install.on.opensuse.xi.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/sh
+
+# This script is used to install sogou-2.4 into openSUSE Leap 15.3 x86_64
+# Script creat time: 2021.06.22, by Oca John <zhangxiyh@yahoo.com>
+
+# Download pkg of the latest release of sogoupinyin(now 2.4)
+wget https://ime.sogouimecdn.com/202106221047/f498062e8b966a3a1875d564dadf2f43/dl/index/1612260778/sogoupinyin_2.4.0.3469_amd64.deb ~/Downloads/
+cd Downloads
+# uncompress the deb pkg, and the data file.
+ar -x sogoupinyin_2.4.0.3469_amd64.deb
+tar -xf data.tar.xz
+
+# Test if sogou-qimpanel is running, by Marguerite Su <marguerite@opensuse.org>
+if test $( pgrep -f sogou-qimpanel | wc -l ) -ne 0 ; then
+	echo "Killing running Sogou Pinyin...";
+	killall sogou-qimpanel;
+	echo "Killing running fcitx...";
+	killall fcitx;
+	echo "Waiting 30s for sogou-qimpanel-watchdog to fully quit...";
+	sleep 30s;
+else
+	echo "Sogou & fcitx are not running, safe to go.";
+fi
+
