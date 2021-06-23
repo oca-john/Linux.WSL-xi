64bef70504a05e44c2f9ea9f0b3ec5169e784771
oca-john <40886062+oca-john@users.noreply.github.com>
2021-06-23 13:37:27 +0800

    Update sogou.install.on.opensuse.xi.sh

--- Linux平台/sogou.install.on.opensuse.xi.sh
+++ Linux平台/sogou.install.on.opensuse.xi.sh
@@ -8,6 +8,7 @@ wget https://ime.sogoucdn.com/dl/index/1612260778/sogoupinyin_2.4.0.3469_amd64.d
 # Uncompress the deb pkg, and the data file.
 ar -x sogoupinyin_2.4.0.3469_amd64.deb
 tar -xf data.tar.xz
+echo "1. Files are prepared ready."
 
 # Test if sogou-qimpanel is running, by Marguerite Su <marguerite@opensuse.org>
 if test $( pgrep -f sogou-qimpanel | wc -l ) -ne 0 ; then
@@ -18,7 +19,7 @@ if test $( pgrep -f sogou-qimpanel | wc -l ) -ne 0 ; then
 	echo "Waiting 30s for sogou-qimpanel-watchdog to fully quit...";
 	sleep 30s;
 else
-	echo "Sogou & fcitx are not running, safe to go.";
+	echo "2. Sogou & fcitx are not running, safe to go.";
 fi
 
 # Installation, File copy
@@ -33,18 +34,21 @@ sudo cp -r usr/share/fcitx/addon/*.conf /usr/share/fcitx/addon/
 sudo cp -r usr/share/fcitx/imicon/*.png /usr/share/fcitx/imicon/
 sudo cp -r usr/share/fcitx/inputmethod/*.conf /usr/share/fcitx/inputmethod/
 sudo cp -ru usr/share/icons/hicolor /usr/share/icons/
+echo "3. Installation files are all dealed."
 
 # Afterwork, by Marguerite Su <marguerite@opensuse.org>
 /usr/bin/update-mime-database /usr/share/mime/packages &> /dev/null
 /usr/bin/update-desktop-database -q /usr/share/applications 
 /usr/bin/gtk-update-icon-cache -fq /usr/share/icons/hicolor
 /sbin/ldconfig
+echo "4. After work is done."
 
 # File clean
 sudo rm -rf etc/ opt/ usr/ control.tar.gz data.tar.xz debian-binary 
 sudo rm sogoupinyin_2.4.0.3469_amd64.deb
+echo "5. Installation files are all cleaned."
 
 # Success
-echo "Sogou Pinyin Linux is now installed."
+echo "Sogoupinyin for Linux is now installed."
 echo "Please start 1) fcitx 2) sogou-qimpanel again."
 exit 0
