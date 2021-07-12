70be6f2f95c73433611d178443bb830e193cc432
oca-john <40886062+oca-john@users.noreply.github.com>
2021-07-12 11:21:53 +0800

    Create output.with.user.define.vars.sh

---
+++ BashScripts/output.with.user.define.vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+website=www.fosslinux.com
+year=2020
+
+# Getting user name from special variables
+name=$USER
+
+echo "Welcome to $website"
+echo -e "Hello $name \n"
+echo -e "Year = $year \n"
+echo "Running on $HOSTNAME"
