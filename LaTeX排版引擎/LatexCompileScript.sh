111d2d4fa6f7e168e4a48de876120067fb751a87
oca-john <40886062+oca-john@users.noreply.github.com>
2021-07-14 19:50:46 +0800

    Update LatexCompileScript.sh

--- LaTeX排版引擎/LatexCompileScript.sh
+++ LaTeX排版引擎/LatexCompileScript.sh
@@ -8,15 +8,20 @@ file=$1
 # 2. 将参数转换为String，使用正则式提取文件名
 # 命令行拼接产生 .tex 文件和 .aux 文件的文件名
 filename=${file%.tex*}
-# echo "$filename\n"
+echo "The file name is gotten.\n"
+echo "It is: $filename\n"
 texfile=$filename.tex
 auxfile=$filename.aux
+echo "Key files had been generated!\n"
 
 # 3. 将相关的文件名嵌入编译工具链，执行编译
 xelatex $texfile    # 生成PDF
 bibtex $auxfile     # 嵌入ref信息
 xelatex $texfile    # 编译ref信息
 xelatex $texfile    # 编译引用位置信息
+echo "Source file had been compiled!\n"
 
 # 4. 清除产生的中间文件
 rm *.aux *.bbl *.blg *.log
+echo "All mid-files had been cleaned!\n"
+echo "I'm oca! Thanks for your choice!\n"
