b37b5c5631f7b766cfc38a8d448e5bb2564544ac
oca-john <40886062+oca-john@users.noreply.github.com>
2021-07-14 19:42:17 +0800

    Create LatexCompileScript.sh

---
+++ LaTeX排版引擎/LatexCompileScript.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+# 本文件将保存为名为`lx`的命令行程序
+# chmod a+x lx; sudo mv lx /usr/bin/
+#
+# 1. 接收主要参数（文件名，含扩展名）
+file=$1
+
+# 2. 将参数转换为String，使用正则式提取文件名
+# 命令行拼接产生 .tex 文件和 .aux 文件的文件名
+filename=${file%.tex*}
+# echo "$filename\n"
+texfile=$filename.tex
+auxfile=$filename.aux
+
+# 3. 将相关的文件名嵌入编译工具链，执行编译
+xelatex $texfile    # 生成PDF
+bibtex $auxfile     # 嵌入ref信息
+xelatex $texfile    # 编译ref信息
+xelatex $texfile    # 编译引用位置信息
+
+# 4. 清除产生的中间文件
+rm *.aux *.bbl *.blg *.log
